f5fb646884168e8e5fc54d9afc47c560090b17f8
terabayashik <terabayashik@mediaedge.co.jp>
2025-03-05 14:48:52 +0900

    Add path sanitization for windows

--- src/ffmpeg.ts
+++ src/ffmpeg.ts
@@ -1,5 +1,5 @@
 import { invoke } from "@tauri-apps/api/core";
-import { BaseDirectory, appCacheDir, join } from "@tauri-apps/api/path";
+import { BaseDirectory, appCacheDir, join, sep } from "@tauri-apps/api/path";
 import { mkdir, readTextFile } from "@tauri-apps/plugin-fs";
 import { Command } from "@tauri-apps/plugin-shell";
 import { type Stats, statsSchema } from "./schema";
@@ -10,13 +10,16 @@ export const analyze = async (refVideo: string, compVideo: string): Promise<Stat
   const outputPath = await join(appCacheDirPath, "vmaf", `vmaf_${Date.now()}.log`);
   const threads = await invoke<number>("get_available_threads");
 
+  const sanitizedOutputPath = sep() === "/" ? outputPath : outputPath.replace(/\\/g, "/").replace(/:/g, "\\:");
+
+  // ref: https://nico-lab.net/libvmaf_with_ffmpeg/#ffmpeg_50
   const command = Command.sidecar("bin/ffmpeg", [
     "-i",
     compVideo,
     "-i",
     refVideo,
     "-lavfi",
-    `libvmaf=log_fmt=json:log_path=${outputPath}:n_threads=${threads}`,
+    `libvmaf=log_fmt=json:log_path='${sanitizedOutputPath}':n_threads=${threads}`,
     "-f",
     "null",
     "-",
